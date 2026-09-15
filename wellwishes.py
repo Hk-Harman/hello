fa1a1d2635c35c29f67dbcf3b7950a5caaef1b32
Hk-Harman <harmankakar9@gmail.com>
2026-09-15 18:12:09 -0400

    Add files via upload

---
+++ wellwishes.py
@@ -0,0 +1,18 @@
+def well_wishes ():
+    print ("hello")
+    print ("how are you?")
+
+well_wishes ()
+
+def hobbies ():
+    print ("hello")
+    print ("what are your hobbies?")
+
+hobbies ()
+
+def introduction ():
+    print ("my name is Harman")
+    print ("how old are you?")
+    print ("do you have any hobbies?")
+
+introduction ()
